0906676751da54ee6adc1d510b88cfa196be07dd
Megarapitor <victorhugodealmeidabezerra@gmail.com>
2025-05-07 17:30:10 -0300

    Update app.js

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,6 +1,6 @@
-let numerosUsados = []
+let numerosUsados = [];
 let numeroMaximo = 10;
-let chutesPartida = []
+let chutesPartida = [];
 let numeroSecreto = getRandomInt(1, numeroMaximo);
 console.log(numeroSecreto);
 let tentativas = 0;
@@ -37,36 +37,36 @@ function getRandomInt(min, max) {
 
 function verificarChute() {
     let chute = parseInt(document.querySelector('input').value);
-    let comparacao = numeroSecreto > chute ? "maior" : "menor"
-    let fimDaLista = 1
-    let inicioDalista = 0
-    let comparacaoChutePartida
+    let comparacao = numeroSecreto > chute ? "maior" : "menor";
+    let fimDaLista = chutesPartida.length - 1;
+    let inicioDalista = 0;
+    let comparacaoChutePartida = numeroSecreto > chutesPartida[0] ? "maior" : "menor";
+    let flexão
     if (isNaN(chute) || chute <= 0 || chute > numeroMaximo) {
         editaTexto("p", `Valor inválido, se atente a valores presentes na escala (1 a ${numeroMaximo})`);
     } else {
         if (chutesPartida.includes(chute)) {
             if (chutesPartida.length == 1) {
-                comparacaoChutePartida = numeroSecreto > chutesPartida[0] ? "maior" : "menor"
-                editaTexto("p", `Ops, você já tentou este número, vamos tentar outro ? Dica: Sabemos que o número secreto é ${comparacaoChutePartida} que ${chutesPartida[0]}`);
+                editaTexto("p", `Ops, você já tentou este número, vamos tentar outro ? Dica: Sabemos que o número secreto é ${comparacaoChutePartida} que ${chutesPartida[inicioDaLista]}`);
             } else {
-                while (chutesPartida[chutesPartida.length - fimDaLista] > numeroSecreto || chutesPartida[inicioDalista] < numeroSecreto) {
-                    if (chutesPartida[chutesPartida.length - fimDaLista] > numeroSecreto) {
-                        fimDaLista++;
+                while (chutesPartida[fimDaLista] > numeroSecreto || chutesPartida[inicioDalista] < numeroSecreto) {
+                    if (chutesPartida[fimDaLista] > numeroSecreto) {
+                        fimDaLista--;
                     }
 
                     if (chutesPartida[inicioDalista] < numeroSecreto) {
                         inicioDalista++;
                     }
                 }
-                fimDaLista -= 1
-                inicioDalista -= 1
+                fimDaLista += 1;
+                inicioDalista -= 1;
                 if (numeroSecreto > chutesPartida[chutesPartida.length - 1]) {
                     editaTexto("p", `Ops, você já tentou este número, vamos tentar outro ? Dica: Sabemos que o número secreto é maior que ${chutesPartida[chutesPartida.length - 1]} `);
                 } else {
                     if (numeroSecreto < chutesPartida[0]) {
                         editaTexto("p", `Ops, você já tentou este número, vamos tentar outro ? Dica: Sabemos que o número secreto é menor que ${chutesPartida[0]}`);
                     } else {
-                        editaTexto("p", `Ops, você já tentou este número, vamos tentar outro ? Dica: Sabemos que o número secreto está entre ${chutesPartida[inicioDalista]} e ${chutesPartida[chutesPartida.length - fimDaLista]}`);
+                        editaTexto("p", `Ops, você já tentou este número, vamos tentar outro ? Dica: Sabemos que o número secreto está entre ${chutesPartida[inicioDalista]} e ${chutesPartida[fimDaLista]}`);
                     }
                 }
             }
@@ -74,18 +74,18 @@ function verificarChute() {
             chutesPartida.push(chute);
             chutesPartida.sort(function (a, b) { return a - b });
             tentativas++;
-            let flexão = tentativas == 1 ? "tentativa" : "tentativas"
+            flexão = tentativas == 1 ? "tentativa" : "tentativas";
             if (chute == numeroSecreto) {
                 editaTexto("h1", "Acertou!!");
                 editaTexto("p", `Você precisou de: ${tentativas} ${flexão}. Vamos jogar mais uma ?`);
-                document.getElementById("reiniciar").removeAttribute("disabled")
-                document.getElementById("chutar").setAttribute("disabled", true)
+                document.getElementById("reiniciar").removeAttribute("disabled");
+                document.getElementById("chutar").setAttribute("disabled", true);
             } else {
                 editaTexto("p", `Errado, mas não desista! Dica: Tente um número ${comparacao}.`);
             }
         }
     }
-    limparCampo()
+    limparCampo();
 }
 function limparCampo() {
     chute = document.querySelector('input');
@@ -93,12 +93,12 @@ function limparCampo() {
 }
 
 function reiniciarJogo() {
-    numeroSecreto = getRandomInt(1, numeroMaximo)
-    limparCampo()
-    mensagemInicial()
-    chutesPartida = []
-    tentativas = 0
-    console.log(numeroSecreto)
-    document.getElementById("reiniciar").setAttribute("disabled", true)
-    document.getElementById("chutar").removeAttribute("disabled")
+    numeroSecreto = getRandomInt(1, numeroMaximo);
+    limparCampo();
+    mensagemInicial();
+    chutesPartida = [];
+    tentativas = 0;
+    console.log(numeroSecreto);
+    document.getElementById("reiniciar").setAttribute("disabled", true);
+    document.getElementById("chutar").removeAttribute("disabled");
 }
